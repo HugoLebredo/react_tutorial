98d2d71b2d83b4be39a86d411cabfe35c67245f2
Hugo Lebredo <hugolebredo@gmail.com>
2022-10-30 20:36:53 +0100

    Ejercicio 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,49 @@
 import { useState } from "react";
 import "./index.css";
 
-const ContadorClicks = ({ cont }) => {
+const DisplayClicks = ({ cont }) => {
   console.log(cont);
   return <h2>{cont}</h2>;
 };
 
-const initState = { left: 0, right: 0 };
+const initState = { left: 0, right: 0, total: 0 };
 
 export default function App() {
   const [clicks, setClicks] = useState(initState);
 
   const handleClickLeft = () => {
-    const newValues = { ...clicks, left: clicks.left + 1 };
+    const newValues = {
+      ...clicks,
+      left: clicks.left + 1,
+      total: clicks.total + 1
+    };
     setClicks(newValues);
   };
 
   const handleClickRight = () => {
-    const newValues = { ...clicks, right: clicks.right + 1 };
+    const newValues = {
+      ...clicks,
+      right: clicks.right + 1,
+      total: clicks.total + 1
+    };
     setClicks(newValues);
   };
 
   return (
-    <div>
+    <div className="App">
       <h1>Trabajar con estados complejos</h1>
-      <p>Tenemos 2 estados independientes uno para el click derecho y otro para el izquierdo</p>
-      <table className="App">
+      <p>
+        {" "}
+        Nuestro estado es un objeto con 3 atributos las funciones handle()
+        aumentan el contador correspondiente y el Total
+        {" "}
+      </p>
+      <p>
+        Chequea la consola!
+      </p>
+      <table className="Table">
         <td>
-          <ContadorClicks cont={clicks.left} />
+          <DisplayClicks cont={clicks.left} />
         </td>
         <td>
           <button onClick={() => handleClickLeft()}>Left Click</button>
@@ -36,9 +52,10 @@ export default function App() {
           <button onClick={() => handleClickRight()}>right Click</button>
         </td>
         <td>
-          <ContadorClicks cont={clicks.right} />
+          <DisplayClicks cont={clicks.right} />
         </td>
       </table>
+      <h3>{`Total de clicks ${clicks.total}`}</h3>
     </div>
   );
 }
